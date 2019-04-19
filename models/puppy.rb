9fa811bfaafadde41f7c418c75bf71d132326d7c
Sammy Wu <swucode@gmail.com>
2019-04-19 04:37:07 +0000

    Automatically backed up by Learn

---
+++ models/puppy.rb
@@ -0,0 +1,20 @@
+class Puppy 
+  
+  attr_accessor :name, :breed, :months_old
+  
+  def name 
+    @name = name
+  end 
+  
+  def breed
+    @breed = breed
+  end 
+  
+  def months_old
+    @months_old = months_old
+  end 
+  
+  def initialize(name, breed, months_old)
+    
+  
+end 
